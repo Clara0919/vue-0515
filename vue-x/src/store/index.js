75c370ddc9d9834fc213565f2c7ba08d9a1c62c3
Clara0919 <mansyuan.he@gmail.com>
2022-05-15 15:02:52 +0800

    4.VueX更新資料（更新）

--- vue-x/src/store/index.js
+++ vue-x/src/store/index.js
@@ -20,6 +20,7 @@ export default createStore({
   },
   mutations: {
     //methods
+    //可以去改變上面state的資料
     setTitle(state, titleData) { //titleData是要儲存傳送給setTitle資料的參數
       state.name = titleData.name
       state.job = titleData.job
@@ -27,6 +28,7 @@ export default createStore({
   },
   actions: {
     //methods
+    //類似mutations但是他不會更改資料，需要透過commit呼叫mutations傳遞出資料
   },
   modules: {
   }
